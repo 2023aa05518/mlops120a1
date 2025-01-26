19738257cdcabb3daaea5ba3ed49b6ba591536fa
anand7079 <55208183+anand7079@users.noreply.github.com>
2025-01-26 10:44:39 +0530

    Update dataset_update.py

--- src/dataset_update.py
+++ src/dataset_update.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # File path of the dataset
 # Replace with your DVC-tracked file path
-dataset_path = "./data/iris_dataset.csv"  
+dataset_path = "./data/iris_dataset.csv"
 
 # Load the existing dataset
 df = pd.read_csv(dataset_path)
@@ -27,5 +27,4 @@ df.to_csv(dataset_path, index=False)
 
 # Print updated shape
 print("Updated dataset shape:", df.shape)
-
 
